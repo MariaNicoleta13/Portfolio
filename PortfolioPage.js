8aafa52486b2c1119d29863f44494640e2a90511
Mary <maria.nicoleta.ion13@gmail.com>
2021-04-18 19:02:21 +0300

    relist

--- PortfolioPage.js
+++ PortfolioPage.js
@@ -4,7 +4,7 @@ var websites = [
     title: "Recipes",
     description:
       "Recipes is an application made with React that aims to help people passionate about cooking to keep track of their favorite recipes and find new ones. The application has many features that come in handy.",
-    languages: ["Html", "Css", "Js", "ReactJS", "json-server", "firebase"],
+    languages: ["Css", "ReactJS", "firebase"],
     githubLink: "https://github.com/MariaNicoleta13/Recipes",
     viewLink: "https://recipes.marys-creation.gallery/",
   },
